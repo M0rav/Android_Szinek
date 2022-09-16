3d40ea1209aedd2c3390e5707c89cd83bdbf85ab
Moravcsik Márk <moravcsikmark@gmail.com>
2022-09-16 10:37:43 +0200

    Fekete háttér piros szöveg

--- app/src/main/java/com/example/szinek/MainActivity.java
+++ app/src/main/java/com/example/szinek/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.szinek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout layout;
+    private TextView textView;
+    private Random rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int red = rnd.nextInt(256);
+                int green = rnd.nextInt(256);
+                int blue = rnd.nextInt(256);
+                layout.setBackgroundColor(Color.rgb(red,green,blue));
+               String szinSzoveges = String.format("%d, %d. %d, red, green, blue");
+                textView.setText(szinSzoveges);
+                if ((red + green + blue)>450){
+                    textView.setTextColor(android.R.color.black);
+
+
+                }
+                else {
+                    textView.setTextColor(android.R.color.black);
+                }
+            }
+        });
+    }
+    private void init(){
+        layout = findViewById(R.id.layout);
+        textView = findViewById(R.id.textSzin);
+        rnd = new Random();
     }
 }
